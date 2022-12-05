bac6e626b6bcec54f6dc4ad1fe3b385a3f74c9c5
Anastasiia Shevtsova <anastasiia08@ukr.net>
2022-12-05 14:03:07 +0100

    baner added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import Header from "./components/Header"
 import SearchBar from "./components/SearchBar"
 import FlightNavigation from "./components/FlightNavigation"
 import FlightsBoard from "./components/FlightsBoard"
-
+import Banner from "./components/Banner"
 function App() {
   return (
     <main className="App">
       <Header />
+      <Banner />
       <SearchBar />
       <FlightNavigation />
       <FlightsBoard />
